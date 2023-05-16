f69ab36711efda777ec619c55695921f86cffb40
JesusMoralesCa <129511503+JesusMoralesCa@users.noreply.github.com>
2023-05-16 12:06:55 +0200

    Delete src/org/foo directory

--- src/org/foo/srcCheckout.groovy
+++
@@ -1,5 +0,0 @@
-class srcCheckout {
-    void run() {
-        checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/JesusMoralesCa/BuildMaven.git']])
-    }
-}
